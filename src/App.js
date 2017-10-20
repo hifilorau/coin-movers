ca1cc21c137ce2c3c8e6d4824bfafa0cd42deb59
hifilorau <tomrau@gmail.com>
2017-10-20 13:41:04 -0400

    checkin style changes

--- src/App.js
+++ src/App.js
@@ -21,31 +21,25 @@ class App extends Component {
       coins:{},
       bigMovers: [],
       bitcoin: {},
-      showBigLosers: false
+      showBigLosers: false,
+      newData: []
     }; // <- set up react state
   }
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    // let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-    // messagesRef.on('child_added', snapshot => {
-    //   /* Update React state when message is added at Firebase Database */
-    //   let message = { text: snapshot.val(), id: snapshot.key };
-    //   this.setState({ messages: [message].concat(this.state.messages) });
-    // })
-
+  componentWillMount() {
      let coinsRef = fire.database().ref('coins').orderByKey().limitToLast(10000);
      coinsRef.on('child_added', snapshot => {
        /* Update React state when message is added at Firebase Database */
-        let newData = { dataset: snapshot.val()};
-        console.log(newData);
+        let newData = { dataset: snapshot.val(), prevKey: snapshot.key};
+       console.log(newData.prevKey);
         let hourlyChange = [...newData.dataset].slice(0);
         hourlyChange.sort(function(a,b) {
           return b.percent_change_1h - a.percent_change_1h;
        })
+
        this.setState({ bigMovers:hourlyChange })
      })
 
-     fire.database().ref('coins').remove();
+    //  fire.database().ref('coins').remove();
 
       getPriceData().then(
         response => {
@@ -85,18 +79,26 @@ class App extends Component {
       });
   }
 
+  sortArray(array) {
+    array.sort(function(a,b) {
+      return b.percent_change_1h - a.percent_change_1h;
+   })
+  }
+
  renderBigMovers() {
-   return this.state.bigMovers.map((data) => {
+   let array = this.state.bigMovers;
+   this.sortArray(array);
+   return array.map((data) => {
      if (this.state.showBigLosers === false && parseInt(data.market_cap_usd, 10) > 1000000 && data.percent_change_1h > 5) {
         return (
           <li key={ data.id } className="movers">
-            <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
                 <div className="center-content">
+                  <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
                       <div className="hourly-change">{data.percent_change_1h + "%"}</div>
                       <div className="coin-name">{data.name}</div>
                       <div className="coin-price">{"$" + data.price_usd}</div>
+                    </a>
                 </div>
-            </a>
             <div className="daily-change">{data.percent_change_24h + "%"}</div>
             <div className="daily-change-label">24hr +/-</div>
             {/* <div className="market-size">{"$" + parseInt(data.market_cap_usd).toFixed(0) } </div> */}
@@ -109,13 +111,13 @@ class App extends Component {
       if ( this.state.showBigLosers === true && parseInt(data.market_cap_usd, 10) > 1000000 && data.percent_change_1h < -5) {
          return (
            <li key={ data.id } className="movers">
-             <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
                  <div className="center-content">
+                   <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
                        <div className="hourly-change">{data.percent_change_1h + "%"}</div>
                        <div className="coin-name">{data.name}</div>
                        <div className="coin-price">{"$" + data.price_usd}</div>
+                     </a>
                  </div>
-             </a>
              <div className="daily-change">{data.percent_change_24h + "%"}</div>
              <div className="daily-change-label">24hr +/-</div>
              {/* <div className="market-size">{"$" + parseInt(data.market_cap_usd).toFixed(0) } </div> */}
@@ -148,10 +150,13 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app-wrapper">
         <div className="header">
           <h1 className="">Coins on the Move</h1>
-          <button onClick={ this.showMovers } className="movers-button">See Movers</button><button onClick= {this.showLosers } className="losers-button">See Losers</button>
+          <div className="filter-wrapper">
+            <button onClick={ this.showMovers } className="movers-button button">See Movers</button>
+            <button onClick= {this.showLosers } className="losers-button button">See Losers</button>
+          </div>
           <div className="main-coin-wrapper">
               <div className="coin-label">Current price of Bitcoin: ${ this.state.bitcoin.price_usd }</div>
               {/* <div className="coin">{ this.state.bitcoin.price_usd }</div> */}
