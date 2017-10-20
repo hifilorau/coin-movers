d28a15e168e0d803a587f1797971398135964593
hifilorau <tomrau@gmail.com>
2017-10-20 11:15:11 -0400

    checkin movers & losers

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
       messages: [],
       coins:{},
       bigMovers: [],
-      bitcoin: {}
+      bitcoin: {},
+      showBigLosers: false
     }; // <- set up react state
   }
   componentWillMount(){
@@ -32,10 +33,11 @@ class App extends Component {
     //   this.setState({ messages: [message].concat(this.state.messages) });
     // })
 
-     let coinsRef = fire.database().ref('coins').orderByKey().limitToLast(100);
+     let coinsRef = fire.database().ref('coins').orderByKey().limitToLast(10000);
      coinsRef.on('child_added', snapshot => {
        /* Update React state when message is added at Firebase Database */
         let newData = { dataset: snapshot.val()};
+        console.log(newData);
         let hourlyChange = [...newData.dataset].slice(0);
         hourlyChange.sort(function(a,b) {
           return b.percent_change_1h - a.percent_change_1h;
@@ -72,9 +74,8 @@ class App extends Component {
             return;
           }
             response.json().then(data => {
-              fire.database().ref('coins').push( data );
+              // fire.database().ref('coins').push( data );
               this.setState({ bitcoin:data[0] });
-              console.log(this.state.bitcoin[0]);
             return;
           });
         }
@@ -84,13 +85,9 @@ class App extends Component {
       });
   }
 
-// createColor() {
-//   return '#'+Math.floor(Math.random()*16777215).toString(16);
-// }
-
  renderBigMovers() {
    return this.state.bigMovers.map((data) => {
-     if (parseInt(data.market_cap_usd) > 1000000 && data.percent_change_1h > 5) {
+     if (this.state.showBigLosers === false && parseInt(data.market_cap_usd, 10) > 1000000 && data.percent_change_1h > 5) {
         return (
           <li key={ data.id } className="movers">
             <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
@@ -108,12 +105,31 @@ class App extends Component {
           </li>
         );
       }
+
+      if ( this.state.showBigLosers === true && parseInt(data.market_cap_usd, 10) > 1000000 && data.percent_change_1h < -5) {
+         return (
+           <li key={ data.id } className="movers">
+             <a className="movers-link" target="_blank" href={"https://coinmarketcap.com/currencies/" + data.id + "/"}>
+                 <div className="center-content">
+                       <div className="hourly-change">{data.percent_change_1h + "%"}</div>
+                       <div className="coin-name">{data.name}</div>
+                       <div className="coin-price">{"$" + data.price_usd}</div>
+                 </div>
+             </a>
+             <div className="daily-change">{data.percent_change_24h + "%"}</div>
+             <div className="daily-change-label">24hr +/-</div>
+             {/* <div className="market-size">{"$" + parseInt(data.market_cap_usd).toFixed(0) } </div> */}
+                         <div className="market-size">{ this.formatMoney(data.market_cap_usd) } </div>
+             <div className="last-updated">{ this.convertEpochToDate(data.last_updated) } </div>
+           </li>
+         );
+       }
   });
  }
 
  formatMoney(n) {
   //  return "$" + parseInt(n).toFixed(0).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-     return formatter.format(parseInt(n));
+     return formatter.format(parseInt(n, 10));
  }
 
  convertEpochToDate(epoch) {
@@ -121,16 +137,13 @@ class App extends Component {
     return moment(dateObj).format('MMM Do, h:mm');
  }
 
-  addMessage(e){
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('messages').push( this.inputEl.value );
-    this.inputEl.value = ''; // <- clear the input
-  }
 
-   calculateCoin = (e) => {
-    e.preventDefault();
-    console.log('query data');
+   showMovers = () => {
+    this.setState( {showBigLosers: false} );
+   }
+
+   showLosers = () => {
+    this.setState( {showBigLosers: true} );
    }
 
   render() {
@@ -138,6 +151,7 @@ class App extends Component {
       <div>
         <div className="header">
           <h1 className="">Coins on the Move</h1>
+          <button onClick={ this.showMovers } className="movers-button">See Movers</button><button onClick= {this.showLosers } className="losers-button">See Losers</button>
           <div className="main-coin-wrapper">
               <div className="coin-label">Current price of Bitcoin: ${ this.state.bitcoin.price_usd }</div>
               {/* <div className="coin">{ this.state.bitcoin.price_usd }</div> */}
--- src/api-calls.js
+++ src/api-calls.js
@@ -1,5 +1,5 @@
 export function getPriceData () {
- return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=1000');
+ return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=5000');
 }
 
 export function getCoinData (id) {
